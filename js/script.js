213dcd29d307f0c7bd1edab7054b66e754778519
kolawole adeyemo <kolawole_adeyemo@yahoo.com>
2023-04-07 19:48:58 +0100

    add PIN to the account

--- js/script.js
+++ js/script.js
@@ -1,27 +1,33 @@
-// //BUSINESS LOGIC
+//BUSINESS LOGIC
+function BankAccount (){
+    this.accounts = {}
+    this.currentId = 0;
+    this.accountBalance = 6500;
+    this.accountNumber = 19952301;
+    this.PIN = 2222
 
-// function BankAccount (){
-//     this.currentId = 0;
-//     this.accountBalance = 6500;
-    //    this.accountNumber = 19952301;
-    // this.PIN = 2222
-
-// }
+}
 
 // ASSIGN CURRENT ID
+BankAccount.prototype.assignid = function (id){
+    this.currentid += 1
+    return this.currentid;
 
-// BankAccount.prototype.assignid = function (id){
-//     this.currentid += 1
-//     return this.currentid;
-
-// }
+}
 
 // ASSIGN ACCOUNT NUMBER
-
 BankAccount.prototype.assignAccountNumber = function(AN){
     this.accountNumber += 800;
     return this.accountNumber;
+
 }
+// FIND ACCOUNT
+BankAccount.prototype.findAccount = function(id) {
+    if (this.accounts[id] != undefined) {
+      return this.accounts[id];
+    }
+    return false;
+  };
 
 // ADD DEPOSIT
 
@@ -31,55 +37,29 @@ BankAccount.prototype.deposit= function(){
 }
 
 // ASSIGN PIN
-BankAccount.prototype.pin = function(){
-    if (this.PIN === undefined){
-        return false;
+BankAccount.prototype.findPin = function(){
+    if (this.PIN === "2222"){
+        return this.accountBalance;
     }
     else{
-        return this.accountBalance;
+        return false;
     }
    
 }
 
-
-
 // SUBTRACT WITHDRAWL
-
 BankAccount.prototype.withdraw = function(){
-    this.accountBalance = this.accountBalance - $("#withdraw").val();
-    return accountBalance;
+    if ($("#withdraw").val() > this.accountBalance){
+        return insufficient;
+    }
+
+    else(this.accountBalance = this.accountBalance - $("#withdraw").val()) 
+
+        return this.accountBalance;   
+  
 }
 
-
-
-// BankAccount.prototype.findAccount = function(id){
-//     if (this.accounts[id] != undefined){
-//         return this.accounts[id];
-//     }
-//     return false;
-// }
-
-
-
-
-// BankAccount.prototype.DeletAccount = function(id){
-//     if (this.accounts[id] === undefined){
-//         return false;
-//     }
-//     delete this.accounts[id];
-//     return true;
-//
-
-// BankAccount.prototype.Balance = function(Balance){
-//     return this.accountBalance
-
-// }
-
-
-
-
 // BUSINESS LOGIC FOR CONTACT
-
 function Account(firstName, lastName, phoneNumber, accountNumber){
 
 this.firstName = firstName;
@@ -89,120 +69,107 @@ this.accountNumber = accountNumber;
 
 };  
 
-
 Account.prototype.accountDetails = function() {
     return this.firstName +" " + this.lastName +" " + this.phoneNumber +" " + this.accountNumber;
 }
 
+//  //deposit button event handler
+//  const deposit_btn = document.getElementById('deposit-btn');
+//  deposit_btn.addEventListener('click', function(){
 
+//      const depositStringToInt = getInputNumb("deposit-amount");
 
-// BUSINESS LOGIC FOR BALANCE 
+//      updateSpanTest("current-deposit", depositStringToInt);
+//      updateSpanTest("current-balance", depositStringToInt);
 
-class BankAccount {
-    Constructor (amount = 2850) {
-        this.amount = amount;
-    }
+//      //setting up the input field blank when clicked
+//      document.getElementById('deposit-amount').value = "";
 
-    balance() {
-        return this.amount
-    }
+//  })
 
-    deposit(amount) {
-        this.amount = this.amount + amount;
-        return this.amount 
+//   //withdraw button event handler
+//   const withdraw_btn = document.getElementById('withdraw-btn');
+//   withdraw_btn.addEventListener('click', function(){
+//      const withdrawNumb = getInputNumb("withdraw-amount");
 
-    }
+//      updateSpanTest("current-withdraw", withdrawNumb);
+//      updateSpanTest("current-balance", -1 * withdrawNumb);
+//      //setting up the input field blank when clicked
+//      document.getElementById('withdraw-amount').value = "";
+//  })
 
-   
+//  //function to parse string input to int
+//  function getInputNumb(idName){
+//      const amount = document.getElementById(idName).value;
+//      const amountNumber = parseFloat(amount);
+//      return amountNumber;
+//  }
 
-    withdraw(amount) {
-        this.amount = this.amount - amount;
-        return this.amount
-    }
+//  function updateSpanTest(idName, addedNumber){
+//      //x1.1 updating balance the same way
+//      const current = document.getElementById(idName).innerText;
+//      const currentStringToInt = parseFloat(current);
 
+//      const total = currentStringToInt + addedNumber;
 
-} 
-
-
-let account = new BankAccount()
-Count.deposit()
-Count.withdraw()
-
-
-
-
-
-
- //deposit button event handler
- const deposit_btn = document.getElementById('deposit-btn');
- deposit_btn.addEventListener('click', function(){
-
-     const depositStringToInt = getInputNumb("deposit-amount");
-
-     updateSpanTest("current-deposit", depositStringToInt);
-     updateSpanTest("current-balance", depositStringToInt);
-
-     //setting up the input field blank when clicked
-     document.getElementById('deposit-amount').value = "";
-
- })
-
-  //withdraw button event handler
-  const withdraw_btn = document.getElementById('withdraw-btn');
-  withdraw_btn.addEventListener('click', function(){
-     const withdrawNumb = getInputNumb("withdraw-amount");
-
-     updateSpanTest("current-withdraw", withdrawNumb);
-     updateSpanTest("current-balance", -1 * withdrawNumb);
-     //setting up the input field blank when clicked
-     document.getElementById('withdraw-amount').value = "";
- })
-
- //function to parse string input to int
- function getInputNumb(idName){
-     const amount = document.getElementById(idName).value;
-     const amountNumber = parseFloat(amount);
-     return amountNumber;
- }
-
- function updateSpanTest(idName, addedNumber){
-     //x1.1 updating balance the same way
-     const current = document.getElementById(idName).innerText;
-     const currentStringToInt = parseFloat(current);
-
-     const total = currentStringToInt + addedNumber;
-
-     //x1.2 setting this value in balance
-     document.getElementById(idName).innerText = total;
- }
+//      //x1.2 setting this value in balance
+//      document.getElementById(idName).innerText = total;
+//  }
 
 
 
+// User Interface Logic ---------
+let bankAccount = new BankAccount();
+
+function displayAccouttDetails(bankAccoutToDisplay) {
+  let accountsList = $("ul#accounts");
+  let htmlForAccountInfo = "";
+  Object.keys(bankAccountToDisplay.Accounts).forEach(function(key) {
+    const account = bankAccountToDisplay.findAccount(key);
+    htmlForAccountInfo += "<li id=" + account.id + ">" + account.firstName + " " + account.lastName+" "+ account.accountNumber + "</li>";
+  });
+
+  accountsList.html(htmlForAccountInfo);
+}
+
+
+function showAccount(accountId) {
+    const account = bankAccount.findAccount(accountId);
+    $("#show-account").show();
+    $(".first-name").html(account.firstName);
+    $(".last-name").html(account.lastName);
+    $(".phone-number").html(account.phoneNumber);
+    $(".account-number").html(account.accountNumber);
+    let buttons = $("#buttons");
+    buttons.empty();
+    buttons.append("<button class='deleteButton' id=" +  + account.id + ">Delete</button>");
+  }
 
 
 
-// // USER INTERFACE LOGIC
-
-// let bankAccount = new BankAccount();
-
-// function displayAccountDetails(bankAccounttoDisplay){
-//     let accountslist = $(" ul#accounts");
-//     let htmlForAccountInfo = "";
-//     Object.keys(bankAccountToDisplay,accounts).forEach(function(key) {
-//         const account = bankAccountToDisplay.findAccount(key); htmlForAccountInfo += "<li id=" + account.id + ">" + account.firstName + " " + account.lastName + "</li>";
-        
-//     });
-
-//     accountslist.html(htmlForAccountInfo);
-
-// }
+  function attachAcountListeners() {
+    $("ul#accounts").on("click", "li", function() {
+      showAccount(this.id);
+      
+    });
+  
+    // to delete contact
+  
+    $("#buttons").on("click", ".deleteButton", function() {
+      bankAccount.deleteContact(this.id);
+      $("#show-account").hide();
+      displayAccountDetails(bankAccount);
+    });
+  }
 
 
 //ACCOUNT OPENING FORM
 
-(document).ready(function(){
+$(document).ready(function(){
     $("#opening-form").submit(function(event){
         event.preventDefault();
+
+        attachAccountListeners();
       
 
         const DateInput = $("#Date").val();
